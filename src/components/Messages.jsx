ade990b2621d135db247bf222dd78b0971a91d04
KuraoHikari <dewaindra705@gmail.com>
2023-01-18 21:13:52 +0800

    update finished app

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Message from './Message';
+import { db } from '../firebase';
+import { ChatContext } from '../context/ChatContext';
+import { doc, onSnapshot } from 'firebase/firestore';
 
 const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, 'chats', data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, [data.chatId]);
+
   return (
     <div className="messages">
-      <Message />
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
     </div>
   );
 };
